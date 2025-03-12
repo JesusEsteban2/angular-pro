f9ca70d9734e661270e3b7e6d053d483e2470c0a
Mananas <Mananas>
2025-03-12 10:11:10 +0100

    Add eslint & prettier

--- projects/demo/src/app/components/header/header.component.ts
+++ projects/demo/src/app/components/header/header.component.ts
@@ -3,7 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'cas-header',
   imports: [],
-  template: `<h1>Welcome {{ title }}</h1>`,
+  template: ` <header>
+    <h1>Welcome {{ title }}!</h1>
+    <ng-content></ng-content>
+  </header>`,
   styleUrl: './header.component.css',
 })
 export class HeaderComponent {
--- projects/demo/src/app/components/menu/menu.component.ts
+++ projects/demo/src/app/components/menu/menu.component.ts
@@ -1,30 +1,79 @@
 import { Component } from '@angular/core';
 
+type MenuItem = {
+  path: string;
+  label: string;
+};
+
 @Component({
   selector: 'cas-menu',
   imports: [],
-  styleUrl: './menu.component.css',
   template: `
-  <nav>
-  <ul>
-  @for (item of items; track $index) {
-    <li>
-    <a [href]="item.path">{{item.label}}</a>
-    </li>
-  }
-  ',
-
+    <nav>
+      <ul>
+        @for (item of items; track item.path) {
+          <li>
+            <a [href]="item.path">{{ item.label }}</a>
+          </li>
+        }
+        @if (isLogin) {
+          <li>
+            <a [href]="'/profile'">Profile</a>
+          </li>
+          <li>
+            <a [href]="'/logout'">Logout</a>
+          </li>
+        } @else {
+          <li>
+            <a [href]="'/login'">Login</a>
+          </li>
+          <li>
+            <a [href]="'/register'">Register</a>
+          </li>
+        }
+      </ul>
+    </nav>
+  `,
+  styles: `
+    :host {
+      display: block;
+    }
+    nav {
+      background-color: #333;
+      color: white;
+    }
+    ul {
+      display: flex;
+      justify-content: space-around;
+      list-style: none;
+      padding: 0;
+    }
+    li {
+      padding: 1rem;
+    }
+    a {
+      color: white;
+      text-decoration: none;
+    }
+  `,
 })
 export class MenuComponent {
-  items = [
-    { path: '/', label: 'Home' },
-    {
-      path: '/about',
-      label: 'About',
-    },
-    {
-      path: '/contact',
-      label: 'Contact',
-    },
-  ];
+  items: MenuItem[];
+  isLogin = false;
+  constructor() {
+    this.items = [
+      {
+        path: '/',
+        label: 'Home',
+      },
+      {
+        path: '/about',
+        label: 'About',
+      },
+      {
+        path: '/contact',
+        label: 'Contact',
+      },
+    ];
+  }
 }
